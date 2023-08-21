1a0852f94346f4982e33f9dbce84ba69c8a68510
nampth <nampth@Nams-MacBook-Air.local>
2023-08-21 10:57:22 +0700

    finish section 8

---
+++ lib/screens/filters.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+// import 'package:meals/screens/tabs.dart';
+// import 'package:meals/widgets/main_drawer.dart';
+
+enum Filter {
+  glutenFree,
+  lacosteFree,
+  vegetarian,
+  vegan,
+}
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key, required this.currentFilter});
+
+  final Map<Filter, bool> currentFilter;
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _FilterScreenState();
+  }
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _glutenFreeFilterSet = false;
+  var _lacosteFreeFilterSet = false;
+  var _vegetarianFreeFilterSet = false;
+  var _veganFreeFilterSet = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _glutenFreeFilterSet = widget.currentFilter[Filter.glutenFree]!;
+    _lacosteFreeFilterSet = widget.currentFilter[Filter.lacosteFree]!;
+    _vegetarianFreeFilterSet = widget.currentFilter[Filter.vegetarian]!;
+    _veganFreeFilterSet = widget.currentFilter[Filter.vegan]!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your filters'),
+      ),
+      // drawer: MainDrawer(
+      //   onSelectScreen: (identifier) {
+      //     Navigator.of(context).pop();
+      //     if (identifier == 'meals') {
+      //       Navigator.of(context).pushReplacement(MaterialPageRoute(
+      //         builder: (ctx) => const TabsScreen(),
+      //       ));
+      //     }
+      //   },
+      // ),
+      body: WillPopScope(
+        onWillPop: () async {
+          Navigator.of(context).pop({
+            Filter.glutenFree: _glutenFreeFilterSet,
+            Filter.lacosteFree: _lacosteFreeFilterSet,
+            Filter.vegetarian: _vegetarianFreeFilterSet,
+            Filter.vegan: _veganFreeFilterSet,
+          });
+          return false;
+        },
+        child: Column(children: [
+          SwitchListTile(
+            value: _glutenFreeFilterSet,
+            onChanged: (newValue) {
+              setState(() {
+                _glutenFreeFilterSet = newValue;
+              });
+            },
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 35, right: 22),
+          ),
+          SwitchListTile(
+            value: _lacosteFreeFilterSet,
+            onChanged: (newValue) {
+              setState(() {
+                _lacosteFreeFilterSet = newValue;
+              });
+            },
+            title: Text(
+              'Lacoste-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include lacoste-free meals',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 35, right: 22),
+          ),
+          SwitchListTile(
+            value: _vegetarianFreeFilterSet,
+            onChanged: (newValue) {
+              setState(() {
+                _vegetarianFreeFilterSet = newValue;
+              });
+            },
+            title: Text(
+              'Vegetarian',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include vegatarian meals',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 35, right: 22),
+          ),
+          SwitchListTile(
+            value: _veganFreeFilterSet,
+            onChanged: (newValue) {
+              setState(() {
+                _veganFreeFilterSet = newValue;
+              });
+            },
+            title: Text(
+              'Lacoste-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include vegan meals',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 35, right: 22),
+          ),
+        ]),
+      ),
+    );
+  }
+}
